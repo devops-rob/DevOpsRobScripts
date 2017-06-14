4eceabd2b42a8a109318fb75d6d00aa9d25ad94d
Rob Barnes <rbarnes@Robs-MBP.home>
2017-06-15 00:03:02 +0100

    Jenkins server deploy script

---
+++ bash scripts/jenkins_installer.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#installer script to install packages using yum for Jenkins server
+
+apps=(
+      	'epel-release'
+        'nano'
+	    'net-tools'
+        'wget'
+	    'vim'
+        'java-1.8.0-openjdk.x86_64'
+        'nginx'
+
+    )
+#the below separates the list entry above to allo for loop to work
+app=$( IFS=$'\n'; echo "${apps[*]}" )
+
+for a in $app
+do
+    yum install $app -y
+
+done
+
+sudo cp /etc/profile /etc/profile_backup
+echo 'export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk' | sudo tee -a /etc/profile
+echo 'export JRE_HOME=/usr/lib/jvm/jre' | sudo tee -a /etc/profile
+source /etc/profile
+
+echo $JAVA_HOME
+echo $JRE_HOME
+
+
+#install repository for Jenkins
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+sudo rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key
+yum install jenkins
+
+#update system with new repo
+yum update
+
+#Start and enable jenkins service
+sudo systemctl start jenkins.service
+sudo systemctl enable jenkins.service
+
+#Firewall config
+sudo firewall-cmd --zone=public --permanent --add-port=8080/tcp
+sudo firewall-cmd --zone=public --permanent --add-service=http
+sudo firewall-cmd --reload
+
+#SELinux disable
+setenforce 0
+
